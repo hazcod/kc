396c9a4abcb141f1919563159da9b7eb2735e421
Niels Hofmans <niels.hofmans@dhl.com>
2017-09-06 16:45:25 +0200

    initial code

---
+++ kc.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+#set -x
+
+keychain="pass"
+
+function getEntries {
+	security dump-keychain -d "${keychain}.keychain" | grep srvr | cut -d '=' -f 2 | awk '{print substr($0, 2, length($0) - 2)}'	
+}
+
+function findEntry {
+	matches=()
+
+	entries=$(getEntries)
+
+	for entry in ${entries[@]}; do
+
+        if [[ $entry == "$1" ]]; then
+            matches+=("$entry")
+            break
+        fi
+            
+        if [[ $entry == *"$1"* ]]; then
+			matches+=("$entry")
+		fi
+	done
+
+	if (( ${#matches[@]} == 0 )); then
+		>&2 echo "No matches found for $1"
+		return
+	fi
+
+	if (( ${#matches[@]} > 1 )); then
+		>&2 echo "Multiple matches:"
+		for match in ${matches[@]}; do
+			>&2 echo "- $match"
+		done
+		return
+	fi
+
+	echo "${matches[0]}"
+}
+
+function getPassword {
+	entry=$(findEntry "$1")
+
+	if [ -z "${entry//}" ]; then
+		return
+	fi
+
+	security find-internet-password -w -s "$entry"
+}
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <website>"
+	exit 1
+fi
+
+getPassword "$1"
